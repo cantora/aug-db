9130ad2246ce046b15ce7567b0e4cf614ba0e09f
anthony cantor <tony.cantor@gmail.com>
2013-05-01 19:46:17 -0600

    improved fifo test
    buffer overlap test is failing

--- test/fifo_test.c
+++ test/fifo_test.c
@@ -94,7 +94,7 @@ void test2() {
 	ok1(fifo_amt(&p1) == 6);
 	ok1(fifo_avail(&p1) == (ARRAY_SIZE(buf1)-6));
 	fifo_consume(&p1, output_buf, 6);
-	ok1(memcmp(test_arr+1, output_buf, 6) == 0);
+	ok1(memcmp(test_arr+1, output_buf, 6*sizeof(int)) == 0);
 	ok1(fifo_amt(&p1) == 0);
 	ok1(fifo_avail(&p1) == (ARRAY_SIZE(buf1)));
 
@@ -103,6 +103,57 @@ void test2() {
 }
 
 void test3() {
+	struct fifo p1;
+	int buf1[256];
+	int output_buf[128];
+	int zero[128];
+	int buf_val[128];
+	size_t i;
+
+	for(i = 0; i < 128; i++) {
+		buf_val[i] = 0xaabbccdd;
+	}
+	fifo_init(&p1, buf1, sizeof(int), 128);
+	ok1(fifo_amt(&p1) == 0);
+	ok1(fifo_avail(&p1) == 128);
+	ok1(p1.read == 0);
+
+	fifo_write(&p1, buf_val, 128);
+	for(i = 0; i < 128; i++) {
+		buf1[128+i] = 0x01234567;
+	}
+	memset(zero, 0, 128*sizeof(int));
+
+	ok1(fifo_amt(&p1) == 128);
+	ok1(fifo_avail(&p1) == 0);
+	ok1(p1.read == 0);
+
+	memset(output_buf, 0, 128);
+	fifo_peek(&p1, output_buf, 128);
+	ok1(memcmp(output_buf, buf_val, 128*sizeof(int)) == 0);
+
+	memset(output_buf, 0, 128*sizeof(int));
+	fifo_consume(&p1, output_buf, 96);
+	ok1(memcmp(output_buf, buf_val, 96*sizeof(int)) == 0);
+	ok1(memcmp(&output_buf[96], zero, 32*sizeof(int)) == 0);
+	ok1(fifo_amt(&p1) == 32);
+	ok1(fifo_avail(&p1) == 96);
+	ok1(p1.read == 96);
+
+	fifo_write(&p1, buf_val, 32);
+	ok1(fifo_amt(&p1) == 64);
+	ok1(fifo_avail(&p1) == 64);
+	ok1(p1.read == 96);
+	
+	memset(output_buf, 0, 128*sizeof(int));
+	fifo_peek(&p1, output_buf, 64);
+	ok1(memcmp(output_buf, buf_val, 64*sizeof(int)) == 0);
+
+#define TEST3AMT 3 + 3 + 1 + 5 + 3 + 1
+	diag("----test3----\n#");
+}
+
+void test4() {
 	struct fifo p1;
 	int buf1[128];
 	int elem;
@@ -114,7 +165,7 @@ void test3() {
 	for(i = 0; i < ARRAY_SIZE(test_arr); i++) {
 		test_arr[i] = rand();
 	}
-	diag("++++test3++++");	
+	diag("++++test4++++");	
 	
 	fifo_init(&p1, buf1, sizeof(int), ARRAY_SIZE(buf1) );
 	ok1(fifo_amt(&p1) == 0);
@@ -124,20 +175,21 @@ void test3() {
 	ok1(fifo_amt(&p1) == 128);
 	ok1(fifo_avail(&p1) == 0);
 	fifo_peek(&p1, output_buf, 128);
-	ok1(memcmp(output_buf, test_arr, 128) == 0);
+	ok1(memcmp(output_buf, test_arr, 128*sizeof(int)) == 0);
 	
-	for(i = 0; i < 128; i++) {
+	for(i = 0; i < 256; i++) {
 		fifo_pop(&p1, &elem);
 		ok1(elem == test_arr[i] && fifo_amt(&p1) == 127 \
 				&& fifo_avail(&p1) == 1);
 		fifo_push(&p1, test_arr+128+i);
 		ok1(fifo_amt(&p1) == 128 && fifo_avail(&p1) == 0);
+		memset(output_buf, 0, 128*sizeof(int));
 		fifo_peek(&p1, output_buf, 128);
-		ok1(memcmp(output_buf, test_arr+i, 128));
+		ok1(memcmp(output_buf, test_arr+i, 128*sizeof(int)));
 	}
 	
-#define TEST3AMT 2 + 3 + 128*3
-	diag("----test3----\n#");
+#define TEST4AMT 2 + 3 + 256*3
+	diag("----test4----\n#");
 }
 
 int main()
@@ -147,9 +199,11 @@ int main()
 	struct test tests[] = {
 		TESTN(1),
 		TESTN(2),
-		TESTN(3)
+		TESTN(3),
+		TESTN(4)
 	};
 
+	total_tests = 0;
 	len = ARRAY_SIZE(tests);
 	for(i = 0; i < len; i++) {
 		total_tests += tests[i].amt;
