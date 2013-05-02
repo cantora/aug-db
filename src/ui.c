cantora/aug-db

commit 6468b067a9736318a0db0850b200e44db4578e31

both if statements based on cmd var so we should use an if/else if

src/ui.c
@@ -275,8 +275,7 @@ static void act_on_state(int *interact_off, int *do_render) {
 		cmd = ui_state_query_run_cmd();
 		if(cmd == UI_QUERY_CMD_CHOOSE)
 			use_chosen_result();
-		
-		if(cmd == UI_QUERY_CMD_EXIT_INTERACT || cmd == UI_QUERY_CMD_CHOOSE)
+		else if(cmd == UI_QUERY_CMD_EXIT_INTERACT || cmd == UI_QUERY_CMD_CHOOSE)
 			*interact_off = 1;
 
 		break;

commit 19a4ea7023d3733df2b6743a4060bc89c6822487

fix bug introduced in commit 6468b067a9736318a0db0850b200e44db4578e31
the second if applies too if cmd == UI_QUERY_CMD_CHOOSE

src/ui.c
@@ -275,7 +275,7 @@ static void act_on_state(int *interact_off, int *do_render) {
 		cmd = ui_state_query_run_cmd();
 		if(cmd == UI_QUERY_CMD_CHOOSE)
 			use_chosen_result();
-		else if(cmd == UI_QUERY_CMD_EXIT_INTERACT || cmd == UI_QUERY_CMD_CHOOSE)
+		if(cmd == UI_QUERY_CMD_EXIT_INTERACT || cmd == UI_QUERY_CMD_CHOOSE)
 			*interact_off = 1;
 
 		break;