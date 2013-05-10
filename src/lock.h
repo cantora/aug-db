5ad76a871e1ca1b9244d0603a4d16f74db4905d5
a. cantor <tony.cantor@gmail.com>
2013-05-10 14:37:20 -0600

    improve lock logging
    also improve macro variable names

--- src/lock.h
+++ src/lock.h
@@ -5,7 +5,7 @@
 #include "api_calls.h"
 
 static inline int fprint_tid(const char *src, const char *func, int lineno, 
-		FILE *f, pthread_t pt, const char *suffix, ...) {
+		pthread_t pt, const char *suffix, ...) {
 	va_list args;
 	size_t k;
 #define FPRINT_TID_BUFLEN ( sizeof(pthread_t)*2 + 1 )
@@ -24,23 +24,23 @@ static inline int fprint_tid(const char *src, const char *func, int lineno,
 	vsnprintf(suf_buf, sizeof(suf_buf), suffix, args);
 	va_end(args);
 
-	return fprintf(f, "(0x%s)%s#%s@%d=> %s\n", buf, src, func, lineno, suf_buf);
+	return aug_log("(0x%s)%s#%s@%d=> %s\n", buf, src, func, lineno, suf_buf);
 }
 
-#define AUG_DB_LOCK(_mtx_ptr, _status, _msg) \
+#define AUG_DB_LOCK(mtx_ptr, status, msg) \
 	do { \
-		fprint_tid(__FILE__, __func__, __LINE__, stderr, pthread_self(), \
-			":lock " stringify(_mtx_ptr) ); \
-		if( (_status = pthread_mutex_lock(_mtx_ptr)) != 0) \
-			{ err_panic(_status, _msg); } \
+		fprint_tid(__FILE__, __func__, __LINE__, pthread_self(), \
+			":lock " stringify(mtx_ptr) ); \
+		if( (status = pthread_mutex_lock(mtx_ptr)) != 0) \
+			{ err_panic(status, msg); } \
 	} while(0)
 
-#define AUG_DB_UNLOCK(_mtx_ptr, _status, _msg) \
+#define AUG_DB_UNLOCK(mtx_ptr, status, msg) \
 	do { \
-		fprint_tid(__FILE__, __func__, __LINE__, stderr, pthread_self(), \
-			":unlock " stringify(_mtx_ptr) ); \
-		if( (_status = pthread_mutex_unlock(_mtx_ptr)) != 0) \
-			{ err_panic(_status, _msg); } \
+		fprint_tid(__FILE__, __func__, __LINE__, pthread_self(), \
+			":unlock " stringify(mtx_ptr) ); \
+		if( (status = pthread_mutex_unlock(mtx_ptr)) != 0) \
+			{ err_panic(status, msg); } \
 	} while(0)
 
 #endif /* AUG_DB_LOCK_H */
