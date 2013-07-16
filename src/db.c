348e415cf9a8c64d0654dabb2e4d307d7d072214
anthony cantor <ar.cantor@gmail.com>
2013-07-15 22:35:49 -0600

    added license headers and LICENSE file

--- src/db.c
+++ src/db.c
@@ -1,3 +1,20 @@
+/* 
+ * Copyright 2013 anthony cantor
+ * This file is part of aug-db.
+ *
+ * aug-db is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * aug-db is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with aug-db.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "db.h"
 #include "err.h"
 #include "api_calls.h"
